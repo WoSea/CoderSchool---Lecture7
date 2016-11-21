d6d376cc1ccd67c6f2f7d633e68e2316330e9075
Quan Vu <nongdenchet8435848@gmail.com>
2016-11-21 16:29:09 +0700

    setup test

--- app/src/main/java/apidez/com/week7/MainActivity.java
+++ app/src/main/java/apidez/com/week7/MainActivity.java
@@ -1,27 +1,15 @@
 package apidez.com.week7;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
-
-import com.jakewharton.rxbinding.widget.RxTextView;
-
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
 
 public class MainActivity extends AppCompatActivity {
     private EditText edtSearch;
     private ListView listView;
     private ProgressBar pbLoading;
-    private Handler handler;
-    private ApiClient apiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,27 +18,5 @@ public class MainActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.listview);
         edtSearch = (EditText) findViewById(R.id.edtSearch);
         pbLoading = (ProgressBar) findViewById(R.id.pbLoading);
-        handler = new Handler();
-        apiClient = new ApiClient();
-
-        // Start observe
-        RxTextView.textChangeEvents(edtSearch)
-                .debounce(300, TimeUnit.MILLISECONDS)
-                .distinctUntilChanged()
-                .map(event -> event.text().toString())
-                .flatMap(keyword -> search(keyword))
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(result -> {
-                    listView.setAdapter(new ArrayAdapter<>(this,
-                            android.R.layout.simple_list_item_1, result));
-                }, throwable -> {
-                    throwable.printStackTrace();
-                });
-    }
-
-    private Observable<List<String>> search(String keyword) {
-        return apiClient.search(keyword)
-                .compose(RxUtils.withSchedulers())
-                .compose(RxUtils.withLoading(handler, pbLoading, listView));
     }
 }
